38ec5a2c699c5f3af603e092fd64836f85520cce
Thanik Bhongbhibhat <email@thanik.me>
2018-03-17 09:39:39 -0700

    Set up path aliases for webpack and jest

--- apollo-client/config/webpack.config.prod.js
+++ apollo-client/config/webpack.config.prod.js
@@ -90,7 +90,9 @@ module.exports = {
     // for React Native Web.
     extensions: ['.web.js', '.mjs', '.js', '.json', '.web.jsx', '.jsx'],
     alias: {
-      
+      '@root': paths.appSrc,
+      '@components': path.join(paths.appSrc, 'components'),
+      '@styles': path.join(paths.appSrc, 'styles'),
       // Support React Native Web
       // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
       'react-native': 'react-native-web',
@@ -121,7 +123,7 @@ module.exports = {
             options: {
               formatter: eslintFormatter,
               eslintPath: require.resolve('eslint'),
-              
+
             },
             loader: require.resolve('eslint-loader'),
           },
@@ -149,7 +151,7 @@ module.exports = {
             include: paths.appSrc,
             loader: require.resolve('babel-loader'),
             options: {
-              
+
               compact: true,
             },
           },
