16fd121e57f0e15941089d14fd46b46cbf17e9c1
Thanik Bhongbhibhat <email@thanik.me>
2018-03-17 13:39:24 -0700

    Connect LinkList to query the feed from server

--- apollo-client/src/components/LinkList.js
+++ apollo-client/src/components/LinkList.js
@@ -1,24 +1,39 @@
 import React from 'react';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
 import Link from './Link';
 
 class LinkList extends React.Component {
   render() {
-    const linksToRender = [
-      {
-        id: '1',
-        description: 'Prisma turns your database into a GraphQL API 😎 😎',
-        url: 'https://www.prismagraphql.com',
-      },
-      {
-        id: '2',
-        description: 'The best GraphQL client',
-        url: 'https://www.apollographql.com/docs/react/',
-      },
-    ];
-    return (
-      <div>{linksToRender.map(link => <Link key={link.id} link={link} />)}</div>
-    );
+    const { feedQuery } = this.props;
+    if (typeof feedQuery === 'undefined') {
+      return null;
+    }
+    if (feedQuery.loading) {
+      return <div>loading</div>;
+    }
+    if (feedQuery.error) {
+      if (process.env.NODE_ENV !== 'production') {
+        // TODO: better error message using graphql error result
+        console.error(feedQuery.error);
+      }
+      return null;
+    }
+    const { feed } = feedQuery;
+    return <div>{feed.map(link => <Link key={link.id} link={link} />)}</div>;
   }
 }
 
-export default LinkList;
+const query = gql`
+  query FeedQuery {
+    feed {
+      id
+      url
+      description
+    }
+  }
+`;
+
+// `option.name` specify props name for the data (default name is "data")
+// then we can access the result via `this.props.feedQuery` from within `LinkList`
+export default graphql(query, { name: 'feedQuery' })(LinkList);
